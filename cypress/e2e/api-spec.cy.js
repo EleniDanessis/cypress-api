2ef014d8706f181c9460946d838dfa74767565ba
Eleni Danessis <e.danessis@gmail.com>
2023-03-12 14:28:28 -0400

    API validate name value

--- cypress/e2e/api-spec.cy.js
+++ cypress/e2e/api-spec.cy.js
@@ -4,7 +4,7 @@ describe('REST API TEST WITH CYPRESS', () => {
 
     // it('API Test-Headers Validation', () => {
 
-    //     cy.request('https://pokeapi.co/api/v2/pokemon/5').as('pokemon')
+    //     cy.request('https://pokeapi.co/api/v2/pokemon/78').as('pokemon')
 
     //     cy.get('@pokemon').its('headers').its('content-type')
     //     .should('include','applilcation/json; charset=utf-8')
@@ -13,10 +13,19 @@ describe('REST API TEST WITH CYPRESS', () => {
 
     it('API Test-Status Validation', () => {
 
-        cy.request('https://pokeapi.co/api/v2/pokemon/5').as('pokemon')
+        cy.request('https://pokeapi.co/api/v2/pokemon/78').as('pokemon')
 
         cy.get('@pokemon').its('status')
         .should('equal', 200)
+
+    })
+
+    it('API Test-validate name value', () => {
+
+        cy.request('https://pokeapi.co/api/v2/pokemon/78').as('pokemon')
+
+        cy.get('@pokemon').its('body')
+        .should('include', {name:'rapidash'})
 
     })
 
